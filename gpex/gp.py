a0e11f07c9be90bae32c0357bb3e978fe61293fb
Erick Matsen <ematsen@gmail.com>
2020-06-30 10:49:16 -0700

    estimating branch lengths and sbn parameters

--- gpex/gp.py
+++ gpex/gp.py
@@ -4,7 +4,10 @@ import libsbn
 
 
 def fit(newick_path, fasta_path):
-    inst = libsbn.rooted_instance("")
-    inst.read_newick_file(newick_path)
+    inst = libsbn.gp_instance("_output/mmap.dat")
     inst.read_fasta_file(fasta_path)
+    inst.read_newick_file(newick_path)
+    inst.make_engine()
     inst.print_status()
+    inst.estimate_branch_lengths(1e-2, 10)
+    inst.estimate_sbn_parameters(1e-2, 10)
