ab1772a401d423e11a03b04671d0c5561b92e86e
Erick Matsen <ematsen@gmail.com>
2020-07-01 15:05:02 -0700

    with branch lengths

--- gpex/iqtree.py
+++ gpex/iqtree.py
@@ -5,5 +5,5 @@ from gpex.utils import shell
 
 def infer(alignment_path, bootstrap_count):
     shell(
-        f"iqtree -m JC -wbt -redo -o outgroup -s {alignment_path} -bb {bootstrap_count}"
+        f"iqtree -m JC -wbtl -redo -o outgroup -s {alignment_path} -bb {bootstrap_count}"
     )
