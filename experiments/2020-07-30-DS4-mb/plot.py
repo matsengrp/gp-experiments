a1ce513227b316933aa184a18beb881f2cf24ba4
Erick Matsen <ematsen@gmail.com>
2020-08-27 12:19:10 -0700

    plotting and output

---
+++ experiments/2020-07-30-DS4-mb/plot.py
@@ -0,0 +1,17 @@
+#! /usr/bin/env python
+
+import matplotlib.pyplot as plt
+import pandas as pd
+import seaborn as sns
+sns.set_context("poster")
+
+gp_df = pd.read_csv("_output/gp-sbn-parameters.csv", names=["gpcsp", "GP probability"])
+sa_df = pd.read_csv("_output/sa-sbn-params.csv", names=["gpcsp", "SA probability"])
+df = pd.merge(gp_df, sa_df)
+# GP should have all of the SA GPCSPs, and also the fake ones.
+assert len(df) == len(sa_df)
+
+ax = sns.scatterplot(x="SA probability", y="GP probability", data=df, alpha=0.2)
+ax.set_aspect(1)
+sns.despine()
+plt.savefig("ds4-comparison.svg", bbox_inches="tight")
